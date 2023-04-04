36fa72cc9fb627caf7050c412aeff8aedf2555d0
Shay Amaro <amarosaa04@gmail.com>
2023-04-04 18:22:08 -0400

    testing

---
+++ terraform_01/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "=3.31.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+    name = "Terraform_Resource_Group"
+    location = "East US"
+}
+
+data "azurerm_resource_group" "call_rg" {
+    resource_group_name = "testRG"
+}
+
