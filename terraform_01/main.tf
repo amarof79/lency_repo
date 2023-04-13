9564c2244d5b2cc2d4c74848923917510d98f9b3
Shay Amaro <amarosaa04@gmail.com>
2023-04-13 10:57:50 -0400

    dot

--- terraform_01/main.tf
+++ terraform_01/main.tf
@@ -11,12 +11,12 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "rg1" {
+resource "azurerm_resource_group" "rg1"{
     name = "Terraform_Resource_Group"
     location = "East US"
 }
 
-data "azurerm_resource_group" "testing" {
+data "azurerm_resource_group" "testing"{
     resource_group_name = "testRG"
 }
 
